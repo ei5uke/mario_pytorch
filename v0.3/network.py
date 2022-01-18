51c5bd8d8ca9e895407c708811213489a4e80262
ei5uke <ehirota13@gmail.com>
2022-01-18 16:26:09 -0500

    feat: changed backprop to only one optimizer instead of two

--- v0.3/network.py
+++ v0.3/network.py
@@ -33,6 +33,7 @@ class Agent(nn.Module):
         )
         self.actor = layer_init(nn.Linear(512, env.action_space.n))
         self.critic = layer_init(nn.Linear(512, 1))
+        #self.optim = optim.Adam(self.parameters(), lr=self.args.lr, eps=1e-5)
         self.args = args
 
     # def get_value(self, x):
@@ -70,6 +71,12 @@ class Agent(nn.Module):
             actions[step] = action
             logprobs[step] = logprob
             dones[step] = next_done
+
+            if "episode" in info.keys():
+                print(f"global_step={self.args.global_step}, episodic_return={info['episode']['r']}")
+                #writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+                #writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+                break
         
         return obs, actions, logprobs, rewards, dones, values
 
@@ -84,7 +91,7 @@ class Agent(nn.Module):
                 idx += 1
         return returns
 
-    def learn(self):
+    def learn(self, optim):
         while self.args.global_step <= self.args.total_timesteps:
             obs, actions, logprobs, rewards, dones, values = self.rollout()
 
@@ -100,11 +107,52 @@ class Agent(nn.Module):
             ratio = torch.exp(log_prob - logprobs.squeeze()).unsqueeze(1)
             actor_loss = (-torch.min(ratio * advantage, torch.clamp(ratio, 1 - self.args.clip, 1 + self.args.clip) * advantage)).mean()
             critic_loss = nn.MSELoss()(values, returns)
+            entropy_loss = entropy.mean()
+            loss = actor_loss - self.args.ent * entropy_loss + critic_loss * self.args.vf
 
-            self.actor_optim.zero_grad()
-            actor_loss.backward(retain_graph=True)
-            self.actor_optim.step()
+            optim.zero_grad()
+            loss.backward()
+            #nn.utils.clip_grad_norm_(self.parameters(), )
+            optim.step()
 
-            self.critic_optim.zero_grad()
-            critic_loss.backward()
-            self.critic_optim.step()
+            # self.actor_optim.zero_grad()
+            # actor_loss.backward(retain_graph=True)
+            # self.actor_optim.step()
+
+            # self.critic_optim.zero_grad()
+            # critic_loss.backward()
+            # self.critic_optim.step()
+
+if __name__ == "__main__":
+    args = parse_args()
+    import gym
+    import time
+    from stable_baselines3.common.atari_wrappers import (
+        ClipRewardEnv,
+        EpisodicLifeEnv,
+        FireResetEnv,
+        MaxAndSkipEnv,
+        NoopResetEnv,
+    )
+    # Frame Modifications
+    env = gym.make(args.gym_id)
+    env = gym.wrappers.RecordEpisodeStatistics(env) # automatically keeps track of rewards and other data
+    #env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4) 
+    env = EpisodicLifeEnv(env)
+    if "FIRE" in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = ClipRewardEnv(env)
+    env = gym.wrappers.ResizeObservation(env, (84, 84))
+    env = gym.wrappers.GrayScaleObservation(env)
+    env = gym.wrappers.FrameStack(env, 4)
+
+    # Our learning
+    agent = Agent(env, args).to(args.device)
+    optim = optim.Adam(agent.parameters(), lr=agent.args.lr, eps=1e-5)
+    agent.learn(optim)
+    print("Finished training")
+    model_name = "ppo_self_1"
+    torch.save(agent.actor.state_dict(), model_name)
+    print(f"Saved model: {model_name}")
