4f42bdd8626bd27dba28956e1ff66567fa5d51dc
ei5uke <ehirota13@gmail.com>
2022-01-21 22:14:43 -0500

    feat: added seeding and changed flags

--- v0.3/arguments.py
+++ v0.3/arguments.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from distutils.util import strtobool
 import torch
 
 def parse_args():
@@ -8,15 +9,19 @@ def parse_args():
     # arguments to set up
     parser.add_argument("--wandb-project-name", type=str, default="breakout-v0",
         help="our wandb project name")
-    parser.add_argument("--exp-name", type=str, default="ppo12",
+    parser.add_argument("--exp-name", type=str, default="ppo14",
         help="the name of this project")
-    parser.add_argument("--gym-id", type=str, default="Breakout-v0",
+    parser.add_argument("--gym-id", type=str, default="BreakoutNoFrameskip-v4",
         help="name of gym environment")
+    parser.add_argument('--seed', type=int, default=1,
+        help='seed of the experiment')
+    parser.add_argument('--torch-deterministic', type=bool, default=True, nargs='?', const=True,
+        help='if toggled, `torch.backends.cudnn.deterministic=False`')
     parser.add_argument("--device", type=str, default=("cuda" if torch.cuda.is_available() else "cpu"),
         help="our device used for calculations")
-    parser.add_argument("--track", type=bool, default=False,
+    parser.add_argument("--track", type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
         help="True -> track with wandb")
-    parser.add_argument("--eval", type=bool, default=False,
+    parser.add_argument("--eval", type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
         help="True -> evaluate our bot and render")
     parser.add_argument("--wandb-entity", type=str, default=None,
         help="entity/team of wandb project")
@@ -25,7 +30,7 @@ def parse_args():
     # arguments for our learning
     parser.add_argument("--lr", type=float, default=2.5e-4,
         help="learning rate of our optimizer")
-    parser.add_argument("--total-timesteps", type=int, default=1000000,
+    parser.add_argument("--total-timesteps", type=int, default=2000000,
         help="total length of learning")
     parser.add_argument("--num-steps", type=int, default=128,
         help="number of steps to run in each rollout")
@@ -43,7 +48,7 @@ def parse_args():
         help="our discount rate")
     # parser.add_argument("--num-minibatches", type=int, default=4,
     #     help="number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=4, # paper says 3, idk why
+    parser.add_argument("--update-epochs", type=int, default=4,
         help="number of epochs to update policy")
     parser.add_argument("--clip", type=float, default=0.1,
         help="our clip constant")
@@ -60,7 +65,7 @@ def parse_args():
 
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
-    args.minibatch_size = int(args.batch_size // args.update_epochs)
+    args.minibatch_size = int(args.batch_size // 4)
     
     return args
 
