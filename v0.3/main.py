79339723a698bdf1bcc508a1ebe1d1f82e8c7c2e
ei5uke <ehirota13@gmail.com>
2022-01-23 23:24:53 -0500

    feat: added recording

--- v0.3/main.py
+++ v0.3/main.py
@@ -10,6 +10,12 @@ from stable_baselines3.common.atari_wrappers import (
     NoopResetEnv,
 )
 
+# SuperMarioBros gym environment
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import gym.wrappers.monitoring.video_recorder
+
 from network import Agent
 from arguments import parse_args
 
@@ -23,15 +29,20 @@ args = parse_args()
 def make_env(gym_id, seed, idx, run_name):
     def thunk():
         env = gym.make(gym_id)
-        env = gym.wrappers.RecordEpisodeStatistics(env) # automatically keeps track of rewards and other data
-        # if idx == 0:
-        #    env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        env = NoopResetEnv(env, noop_max=30)
-        env = MaxAndSkipEnv(env, skip=4) 
-        env = EpisodicLifeEnv(env)
-        if "FIRE" in env.unwrapped.get_action_meanings():
-            env = FireResetEnv(env)
-        env = ClipRewardEnv(env)
+        env = JoypadSpace(env, SIMPLE_MOVEMENT) # only add for gymsupermariobros
+        
+        # probably nice to use but I have it commented out for mario, i used it for mario though
+        #env = gym.wrappers.RecordEpisodeStatistics(env) # automatically keeps track of rewards and other data
+
+        # add these for atari games like breakout
+        # env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4) # this we can keep for mario
+        # env = EpisodicLifeEnv(env)
+        # if "FIRE" in env.unwrapped.get_action_meanings():
+        #    env = FireResetEnv(env)
+        # env = ClipRewardEnv(env)
+
+        # these we can keep for mario and breakout
         env = gym.wrappers.ResizeObservation(env, (84, 84))
         env = gym.wrappers.GrayScaleObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
@@ -91,22 +102,39 @@ def main():
 
     # Evaluation; see it render one environment
     elif args.eval:
+        # record only the first agent, we can choose any, I arbitrarily chose the first one
+        vid = gym.wrappers.monitoring.video_recorder.VideoRecorder(env=envs.envs[0], path=f"videos/{model_name}.mp4")
         obs = envs.reset()
         agent.load_state_dict(torch.load(f"models/{model_name}"))
-        while True:
-            envs.envs[0].render()
-            time.sleep(1/60)
-            obs = torch.Tensor(obs).to(args.device)
-            with torch.no_grad():
-                action, _, _, _ = agent.get_action_and_value(obs)
+        rewards = np.array([0])
+        try:
+            while True:
+                for step in range(args.num_steps):
+                    envs.envs[0].render()
+                    vid.capture_frame()
+                    time.sleep(0.000005)
+                    obs = torch.Tensor(obs).to(args.device)
+                    with torch.no_grad():
+                        action, _, _, _ = agent.get_action_and_value(obs)
 
-            obs, _, _, info = envs.step(action.cpu().numpy())
+                    obs, reward, dones, info = envs.step(action.cpu().numpy())
+                    rewards = np.append(rewards, reward[0])
+                    info = np.array(info).reshape(8,1)
+                    info = info.tolist()
+                    end = [list(info[i][0].values()) for i in range(8)]
+                    lives = [end[i][2] for i in range(8)]
+                    lives = np.array(lives)
 
-            for item in info:
-                if "episode" in item.keys():
-                    print(f"episodic_return={item['episode']['r']}")
-                    break
-
+                    # this can get changed still
+                    # if info[0]["flag_get"]:
+                    #     print("World {} stage {} completed".format(1, 1))
+                    #     break
+                print(np.sum(rewards))
+                rewards = np.array([0])
+                obs = envs.reset()
+                envs.close()
+        except KeyboardInterrupt:
+            envs.close()
     envs.close()
 
 if __name__ == "__main__":
