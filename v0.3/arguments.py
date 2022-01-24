84d0ffc484d6fb3f42e16940a5166d2a7c102902
ei5uke <ehirota13@gmail.com>
2022-01-23 23:25:26 -0500

    feat: tuned hyperparameters for mario

--- v0.3/arguments.py
+++ v0.3/arguments.py
@@ -7,11 +7,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # arguments to set up
-    parser.add_argument("--wandb-project-name", type=str, default="breakout-v0",
+    parser.add_argument("--wandb-project-name", type=str, default="pytorch-mario",
         help="our wandb project name")
-    parser.add_argument("--exp-name", type=str, default="ppo14",
+    parser.add_argument("--exp-name", type=str, default="mario-ppo10",
         help="the name of this project")
-    parser.add_argument("--gym-id", type=str, default="BreakoutNoFrameskip-v4",
+    parser.add_argument("--gym-id", type=str, default="SuperMarioBros-v0",
         help="name of gym environment")
     parser.add_argument('--seed', type=int, default=1,
         help='seed of the experiment')
@@ -28,12 +28,18 @@ def parse_args():
     # parser.add_argument("--capture-video", )
 
     # arguments for our learning
-    parser.add_argument("--lr", type=float, default=2.5e-4,
-        help="learning rate of our optimizer")
-    parser.add_argument("--total-timesteps", type=int, default=2000000,
-        help="total length of learning")
-    parser.add_argument("--num-steps", type=int, default=128,
-        help="number of steps to run in each rollout")
+    # parser.add_argument("--lr", type=float, default=2.5e-4,
+    #     help="learning rate of our optimizer") # for breakout
+    parser.add_argument("--lr", type=float, default=1.0e-4,
+        help="learning rate of our optimizer") # for mario
+    # parser.add_argument("--total-timesteps", type=int, default=10000000,
+    #     help="total length of learning") # for breakout
+    parser.add_argument("--total-timesteps", type=int, default=5000000,
+        help="total length of learning") # for mario
+    # parser.add_argument("--num-steps", type=int, default=128,
+    #     help="number of steps to run in each rollout") # breakout
+    parser.add_argument("--num-steps", type=int, default=512,
+        help="number of steps to run in each rollout") # mario
     parser.add_argument("--global-step", type=int, default=0,
         help="keep track of current step")
     parser.add_argument("--num-envs", type=int, default=8,
@@ -44,28 +50,28 @@ def parse_args():
         help="True -> Use GAE for advantage estimation")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="lambda for GAE")
-    parser.add_argument("--gamma", type=float, default=0.99,
-        help="our discount rate")
+    # parser.add_argument("--gamma", type=float, default=0.99,
+    #     help="our discount rate") # breakout
+    parser.add_argument("--gamma", type=float, default=0.9,
+        help="our discount rate") # mario
     # parser.add_argument("--num-minibatches", type=int, default=4,
-    #     help="number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=4,
-        help="number of epochs to update policy")
+    #     help="number of mini-batches") # breakout
+    parser.add_argument("--num-minibatches", type=int, default=16,
+        help="number of mini-batches") # mario
+    # parser.add_argument("--update-epochs", type=int, default=4,
+    #     help="number of epochs to update policy") # breakout
+    parser.add_argument("--update-epochs", type=int, default=10,
+        help="number of epochs to update policy") # mario
     parser.add_argument("--clip", type=float, default=0.1,
         help="our clip constant")
-    # parser.add_argument("--clip-vloss", type=bool, default=True,
-    #     help="True -> clipped loss for value function")
     parser.add_argument("--ent", type=float, default=0.01,
         help="entropy constant")
     parser.add_argument("--vf", type=float, default=1.0,
         help="value function constant")
-    # parser.add_argument("--max-grad-norm", type=float, default=0.5,
-    #     help="maximum norm for gradient clipping")
-    # parser.add_argument("--target-kl", type=float, default=None,
-    #     help="target KL divergence threshold")
 
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
-    args.minibatch_size = int(args.batch_size // 4)
+    args.minibatch_size = int(args.batch_size // args.num_minibatches)
     
     return args
 
